75e580d9b1094a48080f86ac3631479acabb072e
amrraed <raeramr@gmail.com>
2024-11-19 14:16:16 +0200

    createed the main project

---
+++ Wall Paper changer/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Wall_Paper_changer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.choose = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // choose
+            // 
+            this.choose.BackColor = System.Drawing.Color.Transparent;
+            this.choose.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.choose.FlatAppearance.BorderSize = 2;
+            this.choose.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.choose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.choose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.choose.Location = new System.Drawing.Point(186, 397);
+            this.choose.Name = "choose";
+            this.choose.Size = new System.Drawing.Size(91, 37);
+            this.choose.TabIndex = 1;
+            this.choose.Text = "Select";
+            this.choose.UseVisualStyleBackColor = false;
+            this.choose.Click += new System.EventHandler(this.choose_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.BackgroundImage = global::Wall_Paper_changer.Properties.Resources.CloseBlack;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox3.Location = new System.Drawing.Point(445, 15);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(30, 27);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = global::Wall_Paper_changer.Properties.Resources.minus;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Location = new System.Drawing.Point(409, 15);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(30, 27);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Location = new System.Drawing.Point(-6, -3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(489, 73);
+            this.panel1.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(9, 96);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(460, 266);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(474, 711);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.choose);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button choose;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Wall Paper changer/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Wall_Paper_changer
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int SystemParametersInfo(
+       uint action, uint uParam, string vParam, uint winIni);
+
+        // Constants for the function
+        public static readonly uint SPI_SETDESKWALLPAPER = 0x14;
+        public static readonly uint SPIF_UPDATEINIFILE = 0x01;
+        public static readonly uint SPIF_SENDCHANGE = 0x02;
+
+        // Import SystemParametersInfo function
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool SystemParametersInfo(uint uAction, uint uParam, StringBuilder lpvParam, uint init);
+
+        // Constants for the function
+        private const uint SPI_GETDESKWALLPAPER = 0x0073;
+        private Point mouseLocation;
+
+
+
+       
+        public Form1()
+        {
+            InitializeComponent();
+            StringBuilder wallpaperPath = new StringBuilder(200); // Buffer for wallpaper path
+            bool success = SystemParametersInfo(SPI_GETDESKWALLPAPER, (uint)wallpaperPath.Capacity, wallpaperPath, 0);
+
+            if (success)
+            {
+                pictureBox1.BackgroundImage = Image.FromFile(wallpaperPath.ToString());
+                pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            }
+            else
+            {
+                MessageBox.Show("Failed to retrieve wallpaper path.", "Error");
+            }
+        }
+        private void choose_Click(object sender, EventArgs e)
+        {
+            string filepath = "";
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "PNG Files (*.png)|*.png|JPEG Files (*.jpg;*.jpeg)|*.jpg;*.jpeg|GIF Files (*.gif)|*.gif";
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    filepath = ofd.FileName;
+                }
+            }
+
+            if (!string.IsNullOrEmpty(filepath))
+            {
+                pictureBox1.BackgroundImage = Image.FromFile(filepath);
+                pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+
+                // Show confirmation message box
+                DialogResult result = MessageBox.Show("Do you want to set this image as your wallpaper?", "Confirm Wallpaper Change", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                // Check if the user clicked "Yes"
+                if (result == DialogResult.Yes)
+                {
+                    // Set the wallpaper
+                    SetWallpaper(filepath);
+                    MessageBox.Show("Wallpaper changed successfully!", "Wallpaper Changer", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        public static void SetWallpaper(string path)
+        {
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, path, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Store the mouse position on mouse down
+            mouseLocation = new Point(e.X, e.Y);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            // If left mouse button is held down, move the form
+            if (e.Button == MouseButtons.Left)
+            {
+                // Calculate new form position
+                this.Location = new Point(this.Location.X + e.X - mouseLocation.X,
+                                          this.Location.Y + e.Y - mouseLocation.Y);
+            }
+        }
+    }
+}
